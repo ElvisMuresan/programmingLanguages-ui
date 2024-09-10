458d936284213ed0c640a3eb85a022f868caf0a2
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-09-10 08:06:53 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+import { Button } from "../@/components/ui/button";
+
+
+function App() {
+
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Test Shadcn UI</h1>
+      <Button className="mt-4">Click me</Button>
+    </div>
+  )
+}
+
+export default App
