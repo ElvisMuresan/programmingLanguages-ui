3376b824a603792f14995525f560a32a53fd39bf
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-09-13 20:51:53 +0300

    fix: Fixed the redirect to login without logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
 import { Login } from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -6,35 +11,34 @@ import { ModeToggle } from "./components/mode-toggle";
 import { HomePage } from "./components/HomePage";
 
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <div className="min-h-screen flex flex-col ">
-          <header className="p-4 bg-gray-100 dark:bg-card">
-            <div className="container mx-auto flex justify-end">
-              <ModeToggle />
-            </div>
-          </header>
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<Router>
+				<div className="min-h-screen flex flex-col ">
+					<header className="p-4 bg-gray-100 dark:bg-card">
+						<div className="container mx-auto flex justify-end">
+							<ModeToggle />
+						</div>
+					</header>
 
-          <main className="flex-1">
-            <Routes>
-              <Route path="/login" element={<Login />} /> 
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <HomePage />
-                  </PrivateRoute>
-                }
-              /> 
-              <Route path="*" element={<Navigate to="/login" />} />
-           
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </ThemeProvider>
-  );
+					<main className="flex-1">
+						<Routes>
+							<Route path="/login" element={<Login />} />
+							<Route
+								path="/programming-languages"
+								element={
+									<PrivateRoute>
+										<HomePage />
+									</PrivateRoute>
+								}
+							/>
+							<Route path="*" element={<Navigate to="/login" />} />
+						</Routes>
+					</main>
+				</div>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,105 +1,119 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { useState } from "react"; // Pentru gestionarea erorilor sau stării de autentificare
+import { useEffect, useState } from "react"; // Pentru gestionarea erorilor sau stării de autentificare
 import { loginUser } from "./api/languages-api"; // Importă funcția de login
 
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "./ui/form";
 import { useNavigate } from "react-router-dom";
 
 // Schema de validare cu zod
 const loginSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  password: z.string().min(5, {
-    message: "Password must be at least 5 characters.",
-  }),
+	username: z.string().min(2, {
+		message: "Username must be at least 2 characters.",
+	}),
+	password: z.string().min(5, {
+		message: "Password must be at least 5 characters.",
+	}),
 });
 
 export function Login() {
-  const [error, setError] = useState<string | null>(null); 
-  const navigate = useNavigate()
-  const form = useForm({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  });
+	const [error, setError] = useState<string | null>(null);
+	const navigate = useNavigate();
+	const form = useForm({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			username: "",
+			password: "",
+		},
+	});
 
-  const onSubmit = async (values: z.infer<typeof loginSchema>) => {
-    try {
-      const response = await loginUser(values.username, values.password);
-  
-      if (response && response.token) {
-        localStorage.setItem("token", response.token);
-        localStorage.setItem("username", response.username); 
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			navigate("/programming-languages"); // Redirecționează la HomePage sau orice pagină protejată
+		}
+	}, [navigate]);
 
-        setError(null);  
-        console.log("Login successful!", response);
-        navigate("/")
-      } else {
-        console.log('Full response', response)
-        setError("Login failed. Invalid response from server.");
-      }
-    } catch (error) {
-      setError("Username or password invalid. Please try again.");
-      console.error("Login error: ", error);
-    }
-  };
-  
+	const onSubmit = async (values: z.infer<typeof loginSchema>) => {
+		try {
+			const response = await loginUser(values.username, values.password);
 
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-card">
-      <Card className="w-full max-w-md">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter your username" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder="Enter your password"
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {error && <p className="text-red-500">{error}</p>} {/* Mesaj de eroare */}
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
-  );
+			if (response && response.token) {
+				localStorage.setItem("token", response.token);
+				localStorage.setItem("username", response.username);
+
+				setError(null);
+				console.log("Login successful!", response);
+				navigate("/programming-languages");
+			} else {
+				console.log("Full response", response);
+				setError("Login failed. Invalid response from server.");
+			}
+		} catch (error) {
+			setError("Username or password invalid. Please try again.");
+			console.error("Login error: ", error);
+		}
+	};
+
+	return (
+		<div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-card">
+			<Card className="w-full max-w-md">
+				<CardHeader>
+					<CardTitle>Login</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+							<FormField
+								control={form.control}
+								name="username"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Username</FormLabel>
+										<FormControl>
+											<Input placeholder="Enter your username" {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Password</FormLabel>
+										<FormControl>
+											<Input
+												type="password"
+												placeholder="Enter your password"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							{error && <p className="text-red-500">{error}</p>}{" "}
+							{/* Mesaj de eroare */}
+							<Button type="submit" className="w-full">
+								Login
+							</Button>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
