04871673eb93f231278ca2799a7e8f583502eb29
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-09-15 22:38:06 +0300

    feat: Designed the Add newLanguage page

--- src/components/AddLanguage.tsx
+++ src/components/AddLanguage.tsx
@@ -10,6 +10,7 @@ import {
 	FormMessage,
 } from "./ui/form";
 import { useForm, FormProvider } from "react-hook-form"; // Pentru validare
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 interface FormValues {
 	name: string;
@@ -46,23 +47,25 @@ export const AddNewLanguagePage: React.FC = () => {
 	};
 
 	return (
-		<div className="container mx-auto p-4">
-			<h1 className="text-2xl mb-4">Add New Programming Language</h1>
-
-			{/* Folosim FormProvider pentru a transmite contextul */}
-			<FormProvider {...methods}>
-				<form onSubmit={handleSubmit(onSubmit)}>
+		<div className="flex justify-center items-center h-screen">
+			<Card className="w-full max-w-md">
+				<CardHeader>
+				<CardTitle className="text-center">Add New Programming Language</CardTitle>
+				</CardHeader>
+				<CardContent>
+				<FormProvider {...methods}>
+					<form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
 					{/* Language Name */}
 					<FormField
 						name="name"
 						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Language Name</FormLabel>
-								<FormControl>
-									<Input placeholder="Language Name" {...field} />
-								</FormControl>
-								<FormMessage>{errors.name?.message}</FormMessage>
-							</FormItem>
+						<FormItem>
+							<FormLabel>Language Name</FormLabel>
+							<FormControl>
+							<Input placeholder="Language Name" {...field} />
+							</FormControl>
+							<FormMessage>{errors.name?.message}</FormMessage>
+						</FormItem>
 						)}
 					/>
 
@@ -70,13 +73,13 @@ export const AddNewLanguagePage: React.FC = () => {
 					<FormField
 						name="creator"
 						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Creator</FormLabel>
-								<FormControl>
-									<Input placeholder="Creator" {...field} />
-								</FormControl>
-								<FormMessage>{errors.creator?.message}</FormMessage>
-							</FormItem>
+						<FormItem>
+							<FormLabel>Creator</FormLabel>
+							<FormControl>
+							<Input placeholder="Creator" {...field} />
+							</FormControl>
+							<FormMessage>{errors.creator?.message}</FormMessage>
+						</FormItem>
 						)}
 					/>
 
@@ -84,13 +87,13 @@ export const AddNewLanguagePage: React.FC = () => {
 					<FormField
 						name="releaseYear"
 						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Release Year</FormLabel>
-								<FormControl>
-									<Input type="number" placeholder="Release Year" {...field} />
-								</FormControl>
-								<FormMessage>{errors.releaseYear?.message}</FormMessage>
-							</FormItem>
+						<FormItem>
+							<FormLabel>Release Year</FormLabel>
+							<FormControl>
+							<Input type="number" placeholder="Release Year" {...field} />
+							</FormControl>
+							<FormMessage>{errors.releaseYear?.message}</FormMessage>
+						</FormItem>
 						)}
 					/>
 
@@ -98,38 +101,38 @@ export const AddNewLanguagePage: React.FC = () => {
 					<FormField
 						name="paradigm"
 						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Paradigm</FormLabel>
-								<FormControl>
-									<Input placeholder="Paradigm" {...field} />
-								</FormControl>
-								<FormMessage>{errors.paradigm?.message}</FormMessage>
-							</FormItem>
+						<FormItem>
+							<FormLabel>Paradigm</FormLabel>
+							<FormControl>
+							<Input placeholder="Paradigm" {...field} />
+							</FormControl>
+							<FormMessage>{errors.paradigm?.message}</FormMessage>
+						</FormItem>
 						)}
 					/>
 
+					{/* Popularity */}
 					<FormField
 						name="popularity"
 						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Popularity (%)</FormLabel>
-								<FormControl>
-									<Input
-										type="number"
-										placeholder="Popularity (%)"
-										{...field}
-									/>
-								</FormControl>
-								<FormMessage>{errors.popularity?.message}</FormMessage>
-							</FormItem>
+						<FormItem>
+							<FormLabel>Popularity (%)</FormLabel>
+							<FormControl>
+							<Input type="number" step="0.01" placeholder="Popularity (%)" {...field} />
+							</FormControl>
+							<FormMessage>{errors.popularity?.message}</FormMessage>
+						</FormItem>
 						)}
 					/>
 
-					<Button type="submit" className="w-full p-2">
+					<Button type="submit" className="w-full">
 						Add Language
 					</Button>
-				</form>
-			</FormProvider>
+					</form>
+				</FormProvider>
+				</CardContent>
+			</Card>
 		</div>
+
 	);
 };
